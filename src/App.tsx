f01b757443db6857953ae04d44086ca4849bdca9
mosidrum <mosiokanga@gmail.com>
2024-05-28 16:03:19 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Uploader} from "./components";
+
+function App() {
+  return (
+    <div>
+      <h1>EPUB Metadata Extractor</h1>
+      <Uploader />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Uploader.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Uploader = () => <div>Epub extractor</div>
